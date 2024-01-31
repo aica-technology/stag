3b55ef617f116604fd04debbe6a7f7aebcf4d2e4
JasperTan97 <jastan@student.ethz.ch>
2024-01-31 08:43:42 +0100

    refactor: update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "opencv2/opencv.hpp"
 #include "stag/Stag.h"
+#include "stag/StagDetector.h"
 
 
 int main(int argc, char** argv)
 {
-	cv::Mat image = cv::imread("/path/to/examples/STag-HD23.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat image = cv::imread("/path/to/examples/STag-HD23.jpg", cv::IMREAD_GRAYSCALE);
 
-	stag::Stag stag(23, 7, true);
+	auto corners = std::vector<std::vector<cv::Point2f>>();
+    auto ids = std::vector<int>();
+
+    StagDetector stag(23, 9);
 
 	stag.detectMarkers(image);
 	stag.logResults("/path/to/examples/output/");
